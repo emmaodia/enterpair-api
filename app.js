74e0828018c677ea7e532cd3f6e45b58f20e5297
emmaodia <odia.emma@gmail.com>
2020-01-25 02:14:23 +0100

    Moved db logic from app.js to dbCOnfig.js

--- app.js
+++ app.js
@@ -19,26 +19,7 @@ function connectDatabaseAndStartApp() {
 
 }
 
-//Database Configuration
-const mongoose = require("mongoose");
-const url = "mongodb://localhost:27017/enterpair-api";
-
-mongoose.Promise = global.Promise;
-
-// Connecting to the database
-mongoose.connect( url , {
-    keepAlive: true,
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  })
-  .then(() => {
-      console.log("Successfully connected to the database");
-  }).catch(err => {
-      console.log('Could not connect to the database. Exiting now...');
-      console.log(err)
-      process.exit();
-  });
+const dbConfig = require('./dbConfig');
 
 //Error Handling set up
 app.use((req, res, next) => {
