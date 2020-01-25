86292f06d53639e6ee2687eb90acd2e01234f57f
emmaodia <odia.emma@gmail.com>
2020-01-25 02:06:39 +0100

    Update server.js to include env PORT

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //Server COnfiguration
 const app = require('./app');
-const port = 3000;  
+const port = process.env.PORT || 3000;
+app.set('port', port)  
 const server = http.createServer(app);
 
 server.listen(port, () => console.log(`App running on port ${port}!`))
